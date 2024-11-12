51366363120b884b581a8887035875b671c25c1f
Greed2kk <draiver78@mail.ru>
2024-11-12 16:23:56 +0300

    add prettier and apply format

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-        Hello world!
-    </div>
+    <>
+      <main className='app'>Hello world!</main>
+    </>
   )
 }
 
---
+++ src/components/header/app-header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class AppHeader extends React.Component<{}, {}> {
+  render() {
+    return <header></header>
+  }
+}
+
+export default AppHeader
